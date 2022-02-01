e5a2cdc5fa56427ee838912a4cf0c7d8c870d8da
kundankumark5 <kundankumark5@gmail.com>
2022-02-02 00:41:53 +0530

    for loop for table

---
+++ loop/for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j;
+ printf("This program print table of 2\n");
+ for(i=1;i<=10;i++)
+ {
+ j=2*i;
+ printf("%d\n",j);
+ }
+ return(0);
+}
